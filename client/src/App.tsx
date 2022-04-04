8965fc57dc67c9a522b31f77c0a6d183740d9aa5
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-03 21:27:07 -0400

    feat(client): setup frontend with tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
