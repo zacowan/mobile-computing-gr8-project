09327c544866f17fc25d61d8915677294acf20bf
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-04 20:59:14 -0400

    feat(client/nav): added layout + navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import AppsPage from "./pages/Apps";
+import SettingsPage from "./pages/Settings";
+import NavSidebar from "./components/NavSidebar";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex divide-x">
+      <NavSidebar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/apps" element={<AppsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
     </div>
   );
 };
---
+++ client/src/pages/Apps.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const AppsPage: FC = () => {
+  return (
+    <div>
+      <h1>Apps</h1>
+    </div>
+  );
+};
+
+export default AppsPage;
