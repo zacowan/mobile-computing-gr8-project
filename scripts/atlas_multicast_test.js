ad925f0d849cdbf9bb3fee2efc1136ba018b4418
Shaun Fidler <shaunfidler@ufl.edu>
2022-04-06 17:02:38 -0400

    Work towards a functional discovery feature
    Co-authored-by: Zachary Cowan <zacowan@users.noreply.github.com>
    Co-authored-by: smclaughlin1 <smclaughlin1@users.noreply.github.com>

--- scripts/atlas_multicast_test.js
+++ scripts/atlas_multicast_test.js
@@ -2,6 +2,8 @@ var PORT = 1235;
 var dgram = require("dgram");
 var client = dgram.createSocket("udp4");
 
+let tweets = []
+
 client.on("listening", function () {
   var address = client.address();
   console.log(
@@ -13,10 +15,40 @@ client.on("listening", function () {
 });
 
 client.on("message", function (message, remote) {
-  console.log("A: Epic Command Received. Preparing Relay.");
-  console.log(
-    "B: From: " + remote.address + ":" + remote.port + " - " + message
-  );
+  // console.log("A: Epic Command Received. Preparing Relay.");
+  // console.log(
+  //   "B: From: " + remote.address + ":" + remote.port + " - " + message
+  // );
+  
+  const decodedMessage = JSON.parse(message.toString());
+  let isNewTweet = true;
+   
+  // Check if new message
+  tweets.every((tw) => {
+    let comparableTw = JSON.parse(JSON.stringify(tw));
+    delete comparableTw['lastSeen'];
+    if (JSON.stringify(comparableTw) == JSON.stringify(decodedMessage)) {
+      console.log("Equal message found");
+      isNewTweet = false;
+      tw['lastSeen'] = Date.now();
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+
+  // Add tweet if not new
+  if (isNewTweet) {
+    decodedMessage['lastSeen'] = Date.now();
+    tweets = [...tweets, decodedMessage]
+  }else{
+    
+  }
+
+  console.log(tweets)
+  console.log(tweets.length)
+  
 });
 
 client.bind(PORT);
--- server/routes/discover.js
+++ server/routes/discover.js
@@ -1,20 +1,54 @@
-//Multicast Client receiving sent messages
 var PORT = 1235;
-var MCAST_ADDR = "232.1.1.1"; //same mcast address as Server
-var HOST = '192.168.0.191'; //this is your own IP
-var dgram = require('dgram');
-var client = dgram.createSocket('udp4');
+var dgram = require("dgram");
+var client = dgram.createSocket("udp4");
 
-client.on('listening', function () {
-    var address = client.address();
-    console.log('UDP Client listening on ' + address.address + ":" + address.port);
-    client.setBroadcast(true)
-    client.setMulticastTTL(128); 
-    client.addMembership(MCAST_ADDR);
+let tweets = []
+
+client.on("listening", function () {
+  var address = client.address();
+  console.log(
+    "UDP Client listening on " + address.address + ":" + address.port
+  );
+  client.setBroadcast(true);
+  client.setMulticastTTL(128);
+  client.addMembership("232.1.1.1");
 });
 
-client.on('message', function (message, remote) {   
-    console.log('MCast Msg: From: ' + remote.address + ':' + remote.port +' - ' + message);
+client.on("message", function (message, remote) {
+  // console.log("A: Epic Command Received. Preparing Relay.");
+  // console.log(
+  //   "B: From: " + remote.address + ":" + remote.port + " - " + message
+  // );
+  
+  const decodedMessage = JSON.parse(message.toString());
+  let isNewTweet = true;
+   
+  // Check if new message
+  tweets.every((tw) => {
+    let comparableTw = JSON.parse(JSON.stringify(tw));
+    delete comparableTw['lastSeen'];
+    if (JSON.stringify(comparableTw) == JSON.stringify(decodedMessage)) {
+      console.log("Equal message found");
+      isNewTweet = false;
+      tw['lastSeen'] = Date.now();
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+
+  // Add tweet if not new
+  if (isNewTweet) {
+    decodedMessage['lastSeen'] = Date.now();
+    tweets = [...tweets, decodedMessage]
+  }else{
+    
+  }
+
+  console.log(tweets)
+  console.log(tweets.length)
+  
 });
 
-client.bind(PORT, HOST);
+client.bind(PORT);
