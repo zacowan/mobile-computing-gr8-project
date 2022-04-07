bc370ac5087f00855d7892a9cbfeef7a2b007248
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-07 16:05:16 -0400

    feat(client/react-query): added react-query
    Downgraded react to verison 17

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -9,15 +10,18 @@ import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
+const queryClient = new QueryClient();
+
 const container = document.getElementById("root");
-// @ts-ignore
-const root = createRoot(container);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
+  container
 );
 
 // If you want to start measuring performance in your app, pass a function
