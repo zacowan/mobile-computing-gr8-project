4a8bc475e9abe3381d457a648e68f4d4832db7c8
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-07 16:50:14 -0400

    refactor: autoformatted and removed port

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var apiRouter = require('./routes/api');
+var apiRouter = require("./routes/api");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', apiRouter);
+app.use("/", apiRouter);
 
 module.exports = app;
--- server/discover/discover.js
+++ server/discover/discover.js
@@ -1,17 +1,17 @@
 var PORT = 1235;
 var dgram = require("dgram");
 var client = dgram.createSocket("udp4");
-var redis = require('redis');
+var redis = require("redis");
 
-let tweets = []
+let tweets = [];
 
-connectToRedis = async() =>{
-    await redisClient.connect();
-}
+const connectToRedis = async () => {
+  await redisClient.connect();
+};
 
-tweetsToRedis = async(tweets) => {
-    await redisClient.set('tweets', JSON.stringify(tweets));
-}
+const tweetsToRedis = async (tweets) => {
+  await redisClient.set("tweets", JSON.stringify(tweets));
+};
 
 client.on("listening", function () {
   var address = client.address();
@@ -24,46 +24,41 @@ client.on("listening", function () {
 });
 
 client.on("message", function (message, remote) {
-  
   const decodedMessage = JSON.parse(message.toString());
 
   // Ignore Identity_Entity tweets for simplicity
-  if (decodedMessage['Tweet Type'] == 'Identity_Entity') return;
+  if (decodedMessage["Tweet Type"] == "Identity_Entity") return;
 
   let isNewTweet = true;
-   
+
   // Check if new message
   tweets.every((tw) => {
     let comparableTw = JSON.parse(JSON.stringify(tw));
-    delete comparableTw['lastSeen'];
+    delete comparableTw["lastSeen"];
     if (JSON.stringify(comparableTw) == JSON.stringify(decodedMessage)) {
       console.log("Equal message found");
       isNewTweet = false;
-      tw['lastSeen'] = Date.now();
+      tw["lastSeen"] = Date.now();
       return false;
     } else {
       return true;
     }
   });
 
-
   // Add tweet if not new
   if (isNewTweet) {
-    decodedMessage['lastSeen'] = Date.now();
+    decodedMessage["lastSeen"] = Date.now();
     tweets = [...tweets, decodedMessage];
-  }else{
-    
+  } else {
   }
 
   console.log(tweets);
   console.log(tweets.length);
   tweetsToRedis(tweets);
-  
 });
 
-
 const redisClient = redis.createClient();
-redisClient.on('error', (err) => console.log('Redis Client Error', err));
-connectToRedis()
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+connectToRedis();
 
 client.bind(PORT);
