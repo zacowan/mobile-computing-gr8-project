8a6af536207cf446b340594d5e74c9eb8a78f3c7
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-07 12:11:53 -0400

    feat(client/style): updated styling

--- client/src/pages/home/Services.tsx
+++ client/src/pages/home/Services.tsx
@@ -24,32 +24,36 @@ const ServicesPage: FC = () => {
     <div className="w-full space-y-10">
       <h1 className="text-6xl">Services</h1>
       <SearchBar value={searchTerm} onChange={(val) => setSearchTerm(val)} />
-      <table className="w-full max-w-7xl table-auto border-collapse border border-slate-200 text-left shadow-md">
-        <thead className="border-b border-slate-200">
-          <tr>
-            <th className="py-3 pl-5 font-medium">Name</th>
-            <th className="py-3 pr-5 font-medium">Thing ID</th>
-            <th className="py-3 pr-5 font-medium">Keywords</th>
-            {/* <th className="py-3 pr-5 font-medium"></th> */}
-          </tr>
-        </thead>
-        <tbody className="text-sm text-slate-600">
-          {filteredServices &&
-            filteredServices.map((service, index) => (
-              <tr
-                className="font-light transition-colors hover:bg-slate-100"
-                key={index}
-              >
-                <td className="py-3 pl-5">{service.name}</td>
-                <td className="py-3 pr-5">{service.thingID}</td>
-                <td className="py-3 pr-5">{service.keywords}</td>
-                {/* <td className="py-3 pr-5">
+      <div className="relative max-w-7xl overflow-x-auto rounded border border-slate-200 shadow-md">
+        <table className="w-full table-auto border-collapse text-left text-sm">
+          <thead className="border-b border-slate-200 uppercase">
+            <tr>
+              <th className="py-3 pl-5 font-medium">Name</th>
+              <th className="py-3 pr-5 font-medium">Thing ID</th>
+              <th className="py-3 pr-5 font-medium">Keywords</th>
+              {/* <th className="py-3 pr-5 font-medium">
+                <span className="sr-only">Details</span>
+              </th> */}
+            </tr>
+          </thead>
+          <tbody className="text-slate-600">
+            {filteredServices &&
+              filteredServices.map((service, index) => (
+                <tr
+                  className="font-light transition-colors hover:bg-slate-100"
+                  key={index}
+                >
+                  <td className="py-3 pl-5">{service.name}</td>
+                  <td className="py-3 pr-5">{service.thingID}</td>
+                  <td className="py-3 pr-5">{service.keywords}</td>
+                  {/* <td className="py-3 pr-5">
                   <Link to="">Details</Link>
                 </td> */}
-              </tr>
-            ))}
-        </tbody>
-      </table>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
