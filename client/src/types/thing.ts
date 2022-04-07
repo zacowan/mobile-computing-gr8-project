e0f9b129c89d85e0ef1936e6cdbb8809bed63611
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-07 16:34:54 -0400

    feat(client/fetch): attempts fetch with rq

--- client/src/types/thing.ts
+++ client/src/types/thing.ts
@@ -2,6 +2,7 @@ export type Thing = {
   name: string;
   description: string;
   id: string;
+  smartSpaceID: string;
   ip: string;
   port: number;
 };
---
+++ client/src/utils/apiCalls/utils.ts
@@ -0,0 +1,3 @@
+export const API_HOST = "localhost";
+export const API_PORT = 3001;
+export const API_URL = `http://${API_HOST}:${API_PORT}`;
