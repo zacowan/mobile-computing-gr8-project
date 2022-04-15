1957766401dc8d82e9cabe0629a1d35bf1efa64c
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-15 13:30:02 -0400

    refactor(server/tests)

--- server/utils/codegen/codegen.test.js
+++ server/utils/codegen/codegen.test.js
@@ -10,15 +10,36 @@ const getFileContent = (fname) => {
 };
 
 describe("codegen", () => {
-  it("generates code for a single service call", () => {
-    // Create app
-    const app = new TestApp();
-    app.addServiceComponent();
-    // Generate code file
-    generateCodeFile(app.toObject());
-    // Compare against expected
-    const generated = getFileContent(`${app.id}.py`);
-    const compare = getFileContent(`testFiles/single_service_call.py`);
-    expect(generated).toEqual(compare);
+  describe("single run apps", () => {
+    let app = new TestApp();
+
+    beforeEach(() => {
+      app = new TestApp();
+    });
+
+    it("generates code for a single service call", () => {
+      // Create app
+      app.addServiceComponent();
+      // Generate code file
+      generateCodeFile(app.toObject());
+      // Compare against expected
+      const generated = getFileContent(`${app.id}.py`);
+      const compare = getFileContent(`testFiles/single_service_call.py`);
+      expect(generated).toEqual(compare);
+    });
+  });
+
+  describe("continuous apps", () => {
+    let app = new TestApp();
+
+    beforeEach(() => {
+      app = new TestApp();
+      app.continuous = true;
+      app.loopDelay = 5000;
+    });
+
+    it("generates code for a single service call", () => {
+      expect(1).toEqual(1);
+    });
   });
 });
