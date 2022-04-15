4ee90042767d123c84b6aaa4531154d4b41ebcfe
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-04-15 12:20:18 -0400

    feat(server/format): added prettier config

--- scripts/test_python.py
+++ scripts/test_python.py
@@ -1,5 +1,8 @@
 import requests
 
+URL = "http://localhost:3001/serviceCaller"
+
+
 def main():
     requests.post('url', data={'service': {
         'name': "service_name",
@@ -7,6 +10,7 @@ def main():
         'spaceID': 'space_id',
         'input': ''
     }}).json()
+
 
 if __name__ == "__main__":
     main()
--- server/routes/serviceCaller.js
+++ server/routes/serviceCaller.js
@@ -1,57 +1,46 @@
 var express = require("express");
 var router = express.Router();
-var { setData, getData } = require('../utils/redis');
-var { sendTweet } = require('../utils/atlasServiceHandler');
-var axios = require('axios');
+var { setData, getData } = require("../utils/redis");
+var { sendTweet } = require("../utils/atlasServiceHandler");
+var axios = require("axios");
 
 /**
  * GET (Returns the current Working Directory to the Front-End)
-*/
-router.post("/", async(req, res, next) => {
-    // Get Service Details from Body
-    /*
+ */
+router.post("/", async (req, res, next) => {
+  // Get Service Details from Body
+  /*
     * service: {
-        "name": name, 
+        "name": name,
         "thingID": thingID,
         "spaceID": smartSpaceID,
         "input": singleInput
       }
     */
-    var { service } = req.body;
-    console.log(service)
+  var { service } = req.body;
+  console.log(service);
 
-    var { data: { things } } = await axios.get("http://localhost:3001/discover");
-    console.log(things)
-    var currentThing = things.filter((thing) => {
-      return ((thing.smartSpaceID == service.spaceID) && (thing.id == service.thingID));
-    })[0];
+  var {
+    data: { things },
+  } = await axios.get("http://localhost:3001/discover");
+  console.log(things);
+  var currentThing = things.filter((thing) => {
+    return thing.smartSpaceID == service.spaceID && thing.id == service.thingID;
+  })[0];
 
-    console.log(currentThing)
+  console.log(currentThing);
 
-    sendTweet(currentThing.ip, currentThing.port, currentThing.id, currentThing.smartSpaceID, service.serviceName, service.input);
+  sendTweet(
+    currentThing.ip,
+    currentThing.port,
+    currentThing.id,
+    currentThing.smartSpaceID,
+    service.serviceName,
+    service.input
+  );
 
-    // Return success
-    res.send({"status": "success"})
-  });
-
-/**
- * PUT (Sets the current Working Directory from the Front-End)
-*/
-router.put("/", async(req, res, next) => {
-    // Get the new Working Directory from the Request Body
-    var { workingDir } = req.body;
-    // Set the WorkingDir in Redis
-    await setData("workingDir", workingDir);
-    res.send({"status": "success"});
-  });
-
-/**
- * DELETE (Gets rid of the working directory)
-*/
-router.delete("/", async(req, res, next) => {
-    // Set the WorkingDir in Redis
-    await setData("workingDir", undefined);
-    res.send({"status": "reset"});
-  });
+  // Return success
+  res.send({ status: "success" });
+});
 
 module.exports = router;
